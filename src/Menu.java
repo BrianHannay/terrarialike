2e0eac07eb6c69048c4cc0299c9fbbcc4d93a583
BrianHannay <brian.hannay@itas.ca>
2015-06-06 20:21:53 -0700

    Autocommit from SublimeText2

--- src/Menu.java
+++ src/Menu.java
@@ -14,6 +14,7 @@ public class Menu extends JPanel{
 	
 	public Menu(Container parent){
 		parent.add((JPanel)this);
+		parent.remove(this);
 
 		buttons.add(new MenuButton(Images.MenuNew , new Point(100, 50) , new Point(500, 150)){
 			public void onClick(){
