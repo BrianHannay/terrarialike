1238574812b17ae3019d6ed33b6de0562d7f5247
BrianHannay <brian.hannay@itas.ca>
2015-06-11 00:18:37 -0700

    Autocommit from SublimeText2

--- src/InputHandler.java
+++ src/InputHandler.java
@@ -51,8 +51,8 @@ public class InputHandler implements KeyListener, MouseListener{
 	}
 	public void mouseClicked(MouseEvent e){
 		Point pointCorrectedForInsets = new Point(e.getX() - insetsOffset.x, e.getY() - insetsOffset.y);
-		System.out.println(pointCorrectedForInsets);
-		for (Clickable c : clickables) { 
+		System.out.println(insetsOffset);
+		for (Clickable c : clickables) {  
 			c.tryClick(pointCorrectedForInsets);
 		}
 	}
