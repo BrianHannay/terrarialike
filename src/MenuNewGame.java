7460abd8925076994b4575b09681f2b528a53e7c
BrianHannay <brian.hannay@itas.ca>
2015-06-10 23:21:58 -0700

    Autocommit from SublimeText2

--- src/MenuNewGame.java
+++ src/MenuNewGame.java
@@ -12,6 +12,7 @@ public class MenuNewGame extends JPanel{
 	}
 
 	public void paint(Graphics g){
+		g.drawString("Please enter your name as you would like it to appear on your boarding pass", 50, 50);
 		g.drawImage(Images.TextInput, 100, 50, null);
 	}
 }
