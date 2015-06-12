d4a5eeb4a02bfa0aef0d15b66d22c5b1b1959400
BrianHannay <brian.hannay@itas.ca>
2015-06-11 23:02:33 -0700

    Autocommit from SublimeText2

--- src/MenuNewGame.java
+++ src/MenuNewGame.java
@@ -1,9 +1,14 @@
 import java.awt.*;
 import javax.swing.*;
 import java.awt.event.*;
+import java.awt.Font;
 
 
-public class MenuNewGame extends JPanel implements KeyHandler{
+/**
+** MenuNewGame is part of the menu system for TerrariaLike.
+** It adds itself to graphics object that it is given - object g.
+**/
+public class MenuNewGame extends JPanel{
 
 	private enum State{
 		GET_NAME,
@@ -12,49 +17,70 @@ public class MenuNewGame extends JPanel implements KeyHandler{
 
 	public State currentState = State.GET_NAME;
 	private Container parent;
-	private String nameGiven = "";
-	private static Font inputFont = new Font("Arial", Font.PLAIN, 26);
+	private InputBox nameBox;
 
 	public MenuNewGame(Container parent){
 		this.parent = parent;
+		currentState = State.GET_NAME;
 		parent.add(this);
-		InputHandler.registerKeyHandler(this);
+		nameBox = new InputBox(this);
 	}
 
-	public void pressed(KeyEvent ke){
-		if(ke.getKeyCode() == KeyEvent.VK_ENTER && nameGiven.length() > 0){
-			//player has finished entering their name, continue to character creation
-			currentState = State.GET_PLR;
-			
-		}
-		else if(ke.getKeyCode() == KeyEvent.VK_BACK_SPACE){
-			//this needs to be compared seperately. We don't want a VK_BACK_SPACE appended to the string
-			//if the string length is 0 (VK_BACK_SPACE is a printable character)
-			if(nameGiven.length() > 0){
-				nameGiven = nameGiven.substring(0, nameGiven.length() -1);
-			}
-		}
-		//printable characters are within this range - Sure, the numbers may be magic but they're never going to change.
-		//namegiven may be at most 14 characters.
-		else if(ke.getKeyChar() >= 32 && ke.getKeyChar() < 127 && nameGiven.length() < 14){
-			nameGiven += ke.getKeyChar();
-		}
-		//else ignore input
-	}
+	
 
 	public void paint(Graphics g){
 		switch(currentState){
 			case GET_NAME:
 				g.drawString("Please enter your name as you would like it to appear on your boarding pass", 100, 40);
 				g.drawImage(Images.TextInput, 100, 50, null);
-				g.setFont(inputFont);
-				g.drawString(nameGiven, 110, 90);
 				break;
 			case GET_PLR:
 
 				break;
 			default:
 				ErrorHandler.err(new Exception(), parent, "Unknown state for MenuNewGame; State="+currentState);
+		}
+	}
+
+
+	private class InputBox extends JPanel implements KeyHandler{
+
+		private String nameGiven = "";
+		private Font inputFont = new Font("Arial", Font.PLAIN, 26);
+
+		public InputBox(Container parent){
+			parent.add(this);
+			InputHandler.registerKeyHandler(this);
+		}
+
+		public void pressed(KeyEvent ke){
+			if(ke.getKeyCode() == KeyEvent.VK_ENTER && nameGiven.length() > 0){
+				//player has finished entering their name, continue to character creation
+				currentState = State.GET_PLR;
+
+			}
+			else if(ke.getKeyCode() == KeyEvent.VK_BACK_SPACE){
+				//this needs to be compared seperately. We don't want a VK_BACK_SPACE appended to the string
+				//if the string length is 0 (VK_BACK_SPACE is a printable character)
+				if(nameGiven.length() > 0){
+					nameGiven = nameGiven.substring(0, nameGiven.length() -1);
+				}
+			}
+			//printable characters are within this range - Sure, the numbers may be magic but they're never going to change.
+			//namegiven may be at most 14 characters.
+			else if(ke.getKeyChar() >= 32 && ke.getKeyChar() < 127 && nameGiven.length() < 14){
+				nameGiven += ke.getKeyChar();
+			}
+			//else ignore input
+		}
+
+		public void paint(Graphics g){
+			g.setFont(inputFont);
+			g.drawImage(Images.TextInput, 100, 50, null);
+		}
+
+		public String get(){
+			return nameGiven;
 		}
 	}
 }
