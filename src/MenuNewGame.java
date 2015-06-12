a5edc0d49dcb6e341553d15336450db0c0b810a4
BrianHannay <brian.hannay@itas.ca>
2015-06-11 20:52:08 -0700

    Autocommit from SublimeText2

--- src/MenuNewGame.java
+++ src/MenuNewGame.java
@@ -22,16 +22,17 @@ public class MenuNewGame extends JPanel implements KeyHandler{
 	}
 
 	public void pressed(KeyEvent ke){
-
-		if(ke.getKeyCode() == KeyEvent.VK_BACK_SPACE){
+		if(ke.getKeyCode() == KeyEvent.VK_ENTER && nameGiven.length() > 0){
+			//player has finished entering their name, continue to character creation
+			currentState = State.GET_PLR;
+			
+		}
+		else if(ke.getKeyCode() == KeyEvent.VK_BACK_SPACE){
 			//this needs to be compared seperately. We don't want a VK_BACK_SPACE appended to the string
 			//if the string length is 0 (VK_BACK_SPACE is a printable character)
 			if(nameGiven.length() > 0){
 				nameGiven = nameGiven.substring(0, nameGiven.length() -1);
 			}
-		}
-		else if(ke.getKeyCode() == KeyEvent.VK_ENTER && nameGiven.length() > 0){
-			currentState = State.GET_PLR;
 		}
 		//printable characters are within this range - Sure, the numbers may be magic but they're never going to change.
 		//namegiven may be at most 14 characters.
@@ -50,6 +51,7 @@ public class MenuNewGame extends JPanel implements KeyHandler{
 				g.drawString(nameGiven, 110, 90);
 				break;
 			case GET_PLR:
+
 				break;
 			default:
 				ErrorHandler.err(new Exception(), parent, "Unknown state for MenuNewGame; State="+currentState);
