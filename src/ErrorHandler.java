9735d8ae451ea5dc22cdc5d841fa990c6abfd365
BrianHannay <brian.hannay@itas.ca>
2015-06-23 09:21:20 -0700

    Autocommit from SublimeText2

--- src/ErrorHandler.java
+++ src/ErrorHandler.java
@@ -2,7 +2,7 @@ import java.awt.Container;
 
 public class ErrorHandler{
 	public static void err(Exception e, Container f, String message){
-		//TODO: print the message and Exception to the JFrame
+		//TODO: print the message and Exception to the Container
 		System.out.println(message);
 	}
 }
